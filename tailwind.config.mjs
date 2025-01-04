88c592462514b2f50d2a7b03a9b354c88d4b9b54
thekojopatrick <patrickasare84@gmail.com>
2025-01-04 20:31:08 +0000

    feat: add shimmer animation to ProjectCard component and enhance layout for improved visual presentation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,6 +9,19 @@ export default {
         sans: ["Geist", ...defaultTheme.fontFamily.sans],
         serif: ["Lora", ...defaultTheme.fontFamily.serif],
       },
+      animation: {
+        shimmer: "shimmer 2s linear infinite",
+      },
+      keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
