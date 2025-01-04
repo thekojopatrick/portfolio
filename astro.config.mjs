81942d84b9cba86c410ec90be60375cceaf1303a
thekojopatrick <patrickasare84@gmail.com>
2025-01-04 14:43:42 +0000

    chore: add framer-motion dependency and update global styles for font management

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://kojopatrick.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), react()],
 });
