df8e8268f0e392c65055a13998224f4ac9fbd081
thekojopatrick <patrickasare84@gmail.com>
2025-01-04 11:18:47 +0000

    feat: add Tailwind CSS integration and update global styles
    - Integrated Tailwind CSS into the Astro project by adding the @astrojs/tailwind package.
    - Updated the global CSS file to utilize Tailwind's utility classes, replacing previous styles.
    - Enhanced the global styles with new utility classes for better layout and animations.
    - Updated package.json and pnpm-lock.yaml to include new dependencies for Tailwind CSS.

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Geist", ...defaultTheme.fontFamily.sans],
+        serif: ["Lora", ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
