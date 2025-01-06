63f8cb89a5cc93cdd62b5eb7aee6e1d59639f5cd
thekojopatrick <patrickasare84@gmail.com>
2025-01-06 12:06:24 +0000

    feat: add new Radix UI components and enhance Tailwind animations; update Hero and PageLayout for improved interactivity and visual effects

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,6 +14,9 @@ export default {
         shine: "shine 5s linear infinite",
         "marquee-horizontal": "marquee-x var(--duration) infinite linear",
         "marquee-vertical": "marquee-y var(--duration) linear infinite",
+        "shimmer-slide":
+          "shimmer-slide var(--speed) ease-in-out infinite alternate",
+        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
       },
       keyframes: {
         "marquee-x": {
@@ -35,6 +38,25 @@ export default {
         shine: {
           "0%": { "background-position": "100%" },
           "100%": { "background-position": "-100%" },
+        },
+        "spin-around": {
+          "0%": {
+            transform: "translateZ(0) rotate(0)",
+          },
+          "15%, 35%": {
+            transform: "translateZ(0) rotate(90deg)",
+          },
+          "65%, 85%": {
+            transform: "translateZ(0) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translateZ(0) rotate(360deg)",
+          },
+        },
+        "shimmer-slide": {
+          to: {
+            transform: "translate(calc(100cqw - 100%), 0)",
+          },
         },
       },
     },
