5dba924db8cc4e583e85a1e1239a2c124fbcfb2f
thekojopatrick <patrickasare84@gmail.com>
2025-01-06 10:04:53 +0000

    style: update ProjectCard and ShinyText components for improved visual consistency; adjust button border color and shiny text color for enhanced aesthetics

--- src/components/ui/shiny-text.tsx
+++ src/components/ui/shiny-text.tsx
@@ -16,13 +16,13 @@ const ShinyText = ({
   return (
     <div
       className={cn(
-        `text-[#b5b5b5a4] bg-clip-text inline-block`,
+        `text-[#e4e3e3a4] bg-clip-text inline-block`,
         disabled ? "" : "animate-shine",
         className,
       )}
       style={{
         backgroundImage:
-          "linear-gradient(120deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0) 60%)",
+          "linear-gradient(120deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.9) 50%, rgba(255, 255, 255, 0) 60%)",
         backgroundSize: "200% 100%",
         WebkitBackgroundClip: "text",
         animationDuration: animationDuration,
