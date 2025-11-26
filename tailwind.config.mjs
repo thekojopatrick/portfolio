c578a0811676c3703a09758dcd4ec556b119f09d
thekojopatrick <patrickasare84@gmail.com>
2025-11-26 16:16:08 +0000

    revamp v1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -9,55 +10,8 @@ export default {
         sans: ["Geist", ...defaultTheme.fontFamily.sans],
         serif: ["Instrument Serif", ...defaultTheme.fontFamily.serif],
       },
-      animation: {
-        shimmer: "shimmer 2s linear infinite",
-        shine: "shine 5s linear infinite",
-        "marquee-horizontal": "marquee-x var(--duration) infinite linear",
-        "marquee-vertical": "marquee-y var(--duration) linear infinite",
-        "shimmer-slide":
-          "shimmer-slide var(--speed) ease-in-out infinite alternate",
-        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
-      },
-      keyframes: {
-        "marquee-x": {
-          from: { transform: "translateX(0)" },
-          to: { transform: "translateX(calc(-100% - var(--gap)))" },
-        },
-        "marquee-y": {
-          from: { transform: "translateY(0)" },
-          to: { transform: "translateY(calc(-100% - var(--gap)))" },
-        },
-        shimmer: {
-          from: {
-            backgroundPosition: "0 0",
-          },
-          to: {
-            backgroundPosition: "-200% 0",
-          },
-        },
-        shine: {
-          "0%": { "background-position": "100%" },
-          "100%": { "background-position": "-100%" },
-        },
-        "spin-around": {
-          "0%": {
-            transform: "translateZ(0) rotate(0)",
-          },
-          "15%, 35%": {
-            transform: "translateZ(0) rotate(90deg)",
-          },
-          "65%, 85%": {
-            transform: "translateZ(0) rotate(270deg)",
-          },
-          "100%": {
-            transform: "translateZ(0) rotate(360deg)",
-          },
-        },
-        "shimmer-slide": {
-          to: {
-            transform: "translate(calc(100cqw - 100%), 0)",
-          },
-        },
+      colors: {
+        neutral: colors.neutral,
       },
     },
   },
