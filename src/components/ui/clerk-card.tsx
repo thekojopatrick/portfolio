b742d637938a05081d52cc771dd1f864d364743b
thekojopatrick <patrickasare84@gmail.com>
2025-11-26 21:57:11 +0000

    feat: Update skills and tools content, enhance responsive layout, and add dark mode support.

--- src/components/ui/clerk-card.tsx
+++ src/components/ui/clerk-card.tsx
@@ -11,18 +11,18 @@ export const Card = ({ className, title, children }: CardProps) => {
   return (
     <div
       className={cn(
-        "row-span-1 rounded-xl overflow-hidden group/bento hover:shadow-lg transition duration-200 shadow-input  bg-zinc-50 border justify-between flex flex-col space-y-4",
+        "row-span-1 rounded-xl overflow-hidden group/bento hover:shadow-lg transition duration-200 shadow-input dark:shadow-input-dark bg-zinc-50 dark:bg-zinc-900 border justify-between flex flex-col space-y-4",
         className,
       )}
     >
       {title && (
         <div className="group-hover/bento:translate-x-2 transition duration-200 p-3 pb-0">
-          <div className="font-sans font-semibold text-neutral-600 ">
+          <div className="font-mono font-medium text-neutral-600 dark:text-neutral-50">
             {title}
           </div>
         </div>
       )}
-      <div className="bg-white  border-t rounded-2xl">{children}</div>
+      <div className="bg-white dark:bg-zinc-900 dark:text-neutral-50 border-t rounded-2xl">{children}</div>
     </div>
   );
 };
