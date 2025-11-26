24068367d06a567b05150bec4ee4da42d8fcfa23
thekojopatrick <patrickasare84@gmail.com>
2025-11-26 17:00:29 +0000

    feat: Implement dark mode, add Astro View Transitions, and dynamically load projects from content collections.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ import colors from "tailwindcss/colors";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
