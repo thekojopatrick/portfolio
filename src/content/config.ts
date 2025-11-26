c798b065416e6ee0eba6137d5953a0ac540dae60
thekojopatrick <patrickasare84@gmail.com>
2025-11-26 17:23:40 +0000

    feat: Define content collection schemas and refactor project display to use imported data.

---
+++ src/content/config.ts
@@ -0,0 +1,35 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+
+const projects = defineCollection({
+  type: "content",
+  schema: z.object({
+    category: z.string(),
+    title: z.string(),
+    subtitle: z.string(),
+    description: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    logo: z.string(),
+    date: z.coerce.date(),
+    draft: z.boolean().optional(),
+    demoURL: z.string().optional(),
+    repoURL: z.string().optional(),
+  }),
+});
+
+export const collections = { blog, projects };
